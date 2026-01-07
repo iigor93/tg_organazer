4d2c433b2e91050e57b104d1440cab595bcaccbe
iigor93 <100670993+iigor93@users.noreply.github.com>
2026-01-07 22:00:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
-#ghbdtnggg
+
 from config import TOKEN
 from database.session import engine
 from handlers.cal import handle_calendar_callback, show_calendar
