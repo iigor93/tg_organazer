009b01ef35487912358dccc6dd3167d9137da4e1
Ckobah <ckobah@gmail.com>
2026-01-29 22:01:56 +0300

    2text

--- handlers/contacts.py
+++ handlers/contacts.py
@@ -131,10 +131,20 @@ async def handle_contact(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         elif created_user.is_active:
             text = f"Пользователь {first_name} добавлен в ваши контакты!"
         else:
-            text = f"Пользователь {first_name} добавлен в ваши контакты!\nОтправьте ему приглашение в FamPlanner_bot. Вы сможете добавлять его, как участника события, после того как он нажмет START. Перешлите ему следующее сообщение:\n\nПривет!\nДавай вместе создавать события и строить планы!\nЗапусти бота и нажми START.\nВот ссылка: https://t.me/FamPlanner_bot"
+            text = (
+                f"Пользователь {first_name} добавлен в ваши контакты!\n"
+                "Отправьте ему приглашение в FamPlanner_bot. Вы сможете добавлять его, как участника события, после того как он нажмет START. "
+                "Перешлите ему следующее сообщение:"
+            )
+            invite_text = (
+                "Привет!\nДавай вместе создавать события и строить планы!\n"
+                "Запусти бота и нажми START.\nВот ссылка: https://t.me/FamPlanner_bot"
+            )
 
         await db_controller.get_user(tg_id=update.effective_chat.id)
 
         await update.message.reply_text(text=text)
+        if "invite_text" in locals():
+            await update.message.reply_text(text=invite_text)
     else:
         await update.message.reply_text("Не удалось получить данные пользователя, попробуйте еще раз")
